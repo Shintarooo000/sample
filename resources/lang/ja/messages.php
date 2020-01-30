c3aeb66400d530b37d67d1669ce3ca647299ac6a
shinta <kataoka240407@gmail.com>
2020-01-30 11:55:24 +0000

    youser

---
+++ resources/lang/ja/messages.php
@@ -0,0 +1,10 @@
+<?php
+
+return[
+    'Login' => 'ログイン',
+    'E-Mail Address' => 'メールアドレス',
+    'password' => 'パスワード',
+    'Remember Me' => 'ログイン情報を記憶する',
+    ];
+
+?>
--- routes/web.php
+++ routes/web.php
@@ -25,3 +25,16 @@ Route::group(['prefix' => 'admin'], function(){
     Route::get('profile/create', 'ProfileController@add');
     Route::get('profile/edit', 'ProfileController@edit');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Route::group(['prefix' => 'admin'], function() {
+    Route::get('news/create', 'Admin\NewsController@add')->middleware('auth');
+});
+
+Route::group(['prefix' => 'admin'], function() {
+    Route::get('profile/create', 'Admin\ProfileController@add')->middleware('auth');
+    Route::get('profile/edit', 'Admin\ProfileController@edit')->middleware('auth');
+});
