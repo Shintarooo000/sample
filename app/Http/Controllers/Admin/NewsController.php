e98a662785c096403992cf5ec1b48e0616050466
shinta <kataoka240407@gmail.com>
2020-01-31 06:54:35 +0000

    PHP/Laravel 13

--- app/Http/Controllers/Admin/NewsController.php
+++ app/Http/Controllers/Admin/NewsController.php
@@ -10,4 +10,7 @@ class NewsController extends Controller
  public function add(){
      return view('admin.news.create');
  }
+ public function create(Request $request){
+  return redirect('admin/news/create');
+ }
 }
